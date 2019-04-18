bb016fe84f6f402bec3e07c5c7daec481b2e56af
Guillermo X. Aiken <aikengx@gmail.com>
2019-04-18 15:10:26 -0400

    check

--- dnstoip.js
+++ dnstoip.js
@@ -5,10 +5,12 @@ const domains = require ('./domains.js');
 
 //console.log(domains);
 for (let i = 0; i < domains.length; i++) {
+
   dns.lookup(domains[i] + extension, (err, ip) => {
     console.log(domains[i]+extension+` => ${ip}`);
     // console.log(domains[i] + extension);
     // console.log(ip);
   });
+
 };
 
