7d57a4584455e9be9a5badbdce7fd4982a08edc3
Guillermo X. Aiken <aikengx@gmail.com>
2019-04-18 15:36:32 -0400

    refactored to use map method instead of for loop

--- dnstoip.js
+++ dnstoip.js
@@ -1,16 +1,13 @@
 const dns = require('dns')
 const extension = ".com";
 const domains = require ('./domains.js');
-
-
-//console.log(domains);
-for (let i = 0; i < domains.length; i++) {
-
-  dns.lookup(domains[i] + extension, (err, ip) => {
-    console.log(domains[i]+extension+` => ${ip}`);
-    // console.log(domains[i] + extension);
-    // console.log(ip);
+const domainResolve = (arr) => {
+  arr.map(domain => {
+    dns.lookup(domain + extension, (err, ip) => {
+      console.log(domain + extension + ` => ${ip}`);
+    });
   });
-
 };
 
+
+domainResolve(domains);
