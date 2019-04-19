90b911c79ff3119d70477f873cb3d5107daa0f0d
Guillermo X. Aiken <aikengx@gmail.com>
2019-04-19 11:05:00 -0400

    corrected indentation and spacing

--- dnstoip.js
+++ dnstoip.js
@@ -2,11 +2,13 @@ const dns = require('dns')
 const extension = ".com";
 const domains = require ('./domains.js');
 const domainResolve = (arr) => {
+
   arr.map(domain => {
     dns.lookup(domain + extension, (err, ip) => {
       console.log(domain + extension + ` => ${ip}`);
     });
   });
+
 };
 
 
